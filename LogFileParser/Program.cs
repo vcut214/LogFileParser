3452ccaf450d31005879a0848f2c2f60e0ef279f
vcut214 <26867769+vcut214@users.noreply.github.com>
2021-12-08 00:29:35 +1300

    - Adding sample log file to source control - Adding temporary comment to detail the task specification

--- LogFileParser/Program.cs
+++ LogFileParser/Program.cs
@@ -4,6 +4,14 @@ namespace LogFileParser
 {
     public class Program
     {
+        /*
+         * The task is to parse a log file containing HTTP requests and to report on its contents. For a given log file we
+         *  want to know:
+         *  ● The number of unique IP addresses
+         *  ● The top 3 most visited URLs
+         *  ● The top 3 most active IP addresses
+        */
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
