ff04ef78fdb74738d1fb5974e19777434d0dd72d
vcut214 <26867769+vcut214@users.noreply.github.com>
2021-12-08 00:19:08 +1300

    Add project files.

---
+++ LogFileParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogFileParser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
